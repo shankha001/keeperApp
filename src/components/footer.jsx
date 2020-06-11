9cdda17e4062557f8e508ba1bd547bebb49d4bfc
shankha001 <59788600+shankha001@users.noreply.github.com>
2020-06-11 19:35:17 +0530

    initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const date = new Date();
+const year = date.getFullYear();
+
+
+
+
+
+function Footer(){
+    return <div>
+        <footer>
+            <p>Copyright ⓒ {year}</p>
+        </footer>
+    </div>
+}
+
+export default Footer;
